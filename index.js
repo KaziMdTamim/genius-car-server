a592fadd9f5abbd773e36244c1bffc08fa9ae586
Kazi Md. Tamim <kazimdtamim01@gmail.com>
2022-01-14 00:54:17 +0600

    Hello added

--- index.js
+++ index.js
@@ -66,11 +66,15 @@ async function run() {
     }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server')
 });
+
+app.get('/hello', (req, res) => {
+    res.send('Hello From Heroku')
+})
 
 app.listen(port, () => {
     console.log('Runnig Genius Server On Port', port)
